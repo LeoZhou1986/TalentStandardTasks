9ea1a1056be0b83133bc61bef31d8ef49c6f8967
Leo Zhou <leo.zhou1986@hotmail.com>
2019-09-10 23:33:49 +1200

    Add component EditableTable as a basic profile data table

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Table/EditableTable.jsx
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { Table } from 'semantic-ui-react';
+
+export class EditableTable extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <Table celled striped>
+                <Table.Header>
+                    <Table.Row>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    
+                </Table.Body>
+            </Table>
+        )
+    };
+}
