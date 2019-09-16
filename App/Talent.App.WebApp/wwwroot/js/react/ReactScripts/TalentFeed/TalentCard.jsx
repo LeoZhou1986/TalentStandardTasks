a38a0c552589a5fef447c46b4bf5dc4d46dc0ffd
Leo Zhou <leo.zhou1986@hotmail.com>
2019-09-17 11:05:07 +1200

    Add TalentCard for talent feed to show talent's details

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
@@ -0,0 +1,102 @@
+﻿import React from 'react';
+import ReactPlayer from 'react-player';
+import PropTypes from 'prop-types'
+import { Card, Grid, Icon, List, Button, Image } from 'semantic-ui-react'
+
+export default class TalentCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            detail: false
+        };
+        this.handleClickIcon = this.handleClickIcon.bind(this);
+    };
+
+    handleClickIcon(name) {
+        console.log("Click Icon: ", name);
+        switch (name) {
+            case "user":
+                this.setState({ detail: true });
+                break;
+            case "video":
+                this.setState({ detail: false });
+                break;
+            default:
+                break;
+        }
+    };
+    
+    render() {
+        let skills = ["C#"];
+        let talentDetail;
+        if (this.state.detail) {
+            icons = ["video", "file pdf outline", "linkedin", "github"];
+            talentDetail = <Grid>
+                    <Grid.Column width={8}>
+                    <Image
+                        src='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+                        wrapped
+                    />
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Icon size='big' name='star' />
+                    </Grid.Column>
+            </Grid>
+        } else {
+            icons = ["user", "file pdf outline", "linkedin", "github"];
+            talentDetail = <ReactPlayer
+                url='https://vimeo.com/243556536'
+                className='react-player'
+                width='100%'
+                height='100%'
+            />
+        }
+        return (
+            <Card fluid>
+                <Card.Content>
+                    <Grid>
+                        <Grid.Row>
+                            <Grid.Column width={14}>
+                                <Card.Header><b>Ru(Talent)Ng</b></Card.Header>
+                            </Grid.Column>
+                            <Grid.Column width={2} textAlign='left'>
+                                <Icon size='big' name='star' />
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Card.Content>
+                <Card.Content extra>
+                    {talentDetail}
+                </Card.Content>
+                <Card.Content extra>
+                    <Grid textAlign='center' columns={4}>
+                        <Grid.Row>
+                            {
+                                icons.map((value, index) => <Grid.Column key={index}>
+                                    <Icon
+                                        color='black'
+                                        size='large'
+                                        name={value}
+                                        onClick={() => this.handleClickIcon(value)}
+                                    />
+                                </Grid.Column>)
+                            }
+                        </Grid.Row>
+                    </Grid>
+                </Card.Content>
+                <Card.Content extra>
+                    <List horizontal>
+                        {
+                            skills.map((value, index) => <List.Item key={index}>
+                                <Button basic compact color='blue'>
+                                    {value}
+                                </Button>
+                            </List.Item>)
+                        }
+                    </List>
+                </Card.Content>
+            </Card>
+        )
+    }
+}
+
