4efa166403c7dc1bf215750d61d9e31856df15dc
Leo Zhou <leo.zhou1986@hotmail.com>
2019-09-17 10:59:27 +1200

    Add CompanyProfile for talent feed to show company information

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/CompanyProfile.jsx
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+import { Card, Grid, Image, Icon } from 'semantic-ui-react';
+
+export default class CompanyProfile extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {        
+        return (
+            <Card>
+                <Card.Content>
+                    <Grid centered>
+                        <Grid.Row>
+                            <Image
+                                style={{ width: '48px', height: '48px' }}
+                                circular
+                                src='https://react.semantic-ui.com/images/wireframe/image.png'
+                                centered
+                            />
+                        </Grid.Row>
+                    </Grid>
+                    <Card.Header textAlign='center'>MVP Studio</Card.Header>
+                    <Card.Meta textAlign='center'>
+                        <Icon name='marker' />
+                         Auckland, NewZealand
+                    </Card.Meta>
+                    <Card.Description textAlign='center'>
+                        We currently do not have specific skills that we desire.
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Card.Meta>
+                        <Icon name='phone' />
+                        : 232323
+                    </Card.Meta>
+                    <Card.Meta>
+                        <Icon name='mail' />
+                        : ru@mvp.studio
+                    </Card.Meta>
+                </Card.Content>
+            </Card>
+        )
+    };
+}
