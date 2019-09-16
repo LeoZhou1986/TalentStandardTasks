0d52182b73f5f135846886a28fc564fdf597fb33
Leo Zhou <leo.zhou1986@hotmail.com>
2019-09-17 11:07:16 +1200

    Update TalentFeed UI display

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
@@ -0,0 +1,72 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie'
+import TalentCard from '../TalentFeed/TalentCard.jsx';
+import { Loader, Grid, Container } from 'semantic-ui-react';
+import CompanyProfile from '../TalentFeed/CompanyProfile.jsx';
+import FollowingSuggestion from '../TalentFeed/FollowingSuggestion.jsx';
+import { BodyWrapper, loaderData } from '../Layout/BodyWrapper.jsx';
+
+export default class TalentFeed extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let loader = loaderData
+        loader.allowedUsers.push("Employer")
+        loader.allowedUsers.push("Recruiter")
+
+        this.state = {
+            loadNumber: 5,
+            loadPosition: 0,
+            feedData: [],
+            watchlist: [],
+            loaderData: loader,
+            loadingFeedData: false,
+            companyDetails: null
+        }
+
+        this.init = this.init.bind(this);
+
+    };
+
+    init() {
+        let loaderData = TalentUtil.deepCopy(this.state.loaderData)
+        loaderData.isLoading = false;
+        this.setState({ loaderData });//comment this
+    }
+
+    componentDidMount() {
+        //window.addEventListener('scroll', this.handleScroll);
+        this.init()
+    };
+
+   
+    render() {
+
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
+                <div className="ui container">
+                    <Grid celled centered>
+                        <Grid.Row>
+                            <Grid.Column width={4}>
+                                <Container>
+                                    <CompanyProfile/>
+                                </Container>
+                            </Grid.Column>
+                            <Grid.Column width={8}>
+                                <Container>
+                                    <TalentCard/>
+                                </Container>
+                            </Grid.Column>
+                            <Grid.Column width={4}>
+                                <Container>
+                                    <FollowingSuggestion/>
+                                </Container>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            </BodyWrapper>
+        )
+    }
+}
