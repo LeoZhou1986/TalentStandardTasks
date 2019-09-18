d6da733249ac36db815d82c8f4f20093403ad800
Leo Zhou <leo.zhou1986@hotmail.com>
2019-09-18 12:00:01 +1200

    Update GetTalentSnapshotList function

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
@@ -1,11 +1,13 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 import Cookies from 'js-cookie'
-import TalentCard from '../TalentFeed/TalentCard.jsx';
 import { Loader, Grid, Container } from 'semantic-ui-react';
+import InfiniteScroll from 'react-infinite-scroller';
+import TalentCard from '../TalentFeed/TalentCard.jsx';
 import CompanyProfile from '../TalentFeed/CompanyProfile.jsx';
 import FollowingSuggestion from '../TalentFeed/FollowingSuggestion.jsx';
 import { BodyWrapper, loaderData } from '../Layout/BodyWrapper.jsx';
+
 
 export default class TalentFeed extends React.Component {
     constructor(props) {
@@ -17,16 +19,56 @@ export default class TalentFeed extends React.Component {
 
         this.state = {
             loadNumber: 5,
-            loadPosition: 0,
+            loadPosition: 220,
             feedData: [],
+            hasMoreFeedData: true,
             watchlist: [],
             loaderData: loader,
             loadingFeedData: false,
             companyDetails: null
         }
-
         this.init = this.init.bind(this);
+        this.getTalentSnapshotList = this.getTalentSnapshotList.bind(this);
+    };
 
+    getTalentSnapshotList(position, number) {
+        if (this.state.loadingFeedData || !this.state.hasMoreFeedData) return;
+        this.setState({ loadingFeedData: true });
+        var cookies = Cookies.get('talentAuthToken');
+        $.ajax({
+            url: `http://localhost:60290/profile/profile/getTalent?position=${position}&number=${number}`,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            success: function (res) {
+                if (res.success) {
+                    if (res.data) {
+                        this.setState({
+                            loadPosition: position + res.data.length,
+                            feedData: this.state.feedData.concat(res.data),
+                            hasMoreFeedData: true,
+                            loadingFeedData: false
+                        });
+                    } else {
+                        this.setState({
+                            hasMoreFeedData: false,
+                            loadingFeedData: false
+                        });
+                    }
+                } else {
+                    TalentUtil.notification.show(res.message, "error", null, null);
+                    this.setState({ hasMoreFeedData: false, loadingFeedData: false });
+                }
+            }.bind(this),
+            error: function (res) {
+                TalentUtil.notification.show("Can't fetch talent data: " + res.status, "error", null, null);
+                this.setState({ loadingFeedData: false });
+            }.bind(this)
+        }); 
     };
 
     init() {
@@ -36,17 +78,18 @@ export default class TalentFeed extends React.Component {
     }
 
     componentDidMount() {
-        //window.addEventListener('scroll', this.handleScroll);
-        this.init()
+        //http://localhost:60290/profile/profile/getEmployerProfile
+        
+        this.init();
+        this.getTalentSnapshotList(this.state.loadPosition, this.state.loadNumber);
     };
 
-   
     render() {
-
+        console.log("Talent Feed: ", this.state);
         return (
             <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
                 <div className="ui container">
-                    <Grid celled centered>
+                    <Grid centered>
                         <Grid.Row>
                             <Grid.Column width={4}>
                                 <Container>
@@ -55,7 +98,16 @@ export default class TalentFeed extends React.Component {
                             </Grid.Column>
                             <Grid.Column width={8}>
                                 <Container>
-                                    <TalentCard/>
+                                    <InfiniteScroll
+                                        pageStart={0}
+                                        loadMore={() => this.getTalentSnapshotList(this.state.loadPosition, this.state.loadNumber)}
+                                        hasMore={this.state.hasMoreFeedData}
+                                        loader={<div className="loader" key={0}>Loading ...</div>}
+                                    >
+                                        {
+                                            this.state.feedData.map((value, index) => <TalentCard key={index} />)
+                                        }
+                                    </InfiniteScroll>
                                 </Container>
                             </Grid.Column>
                             <Grid.Column width={4}>
@@ -63,6 +115,8 @@ export default class TalentFeed extends React.Component {
                                     <FollowingSuggestion/>
                                 </Container>
                             </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
                         </Grid.Row>
                     </Grid>
                 </div>
--- Talent.Services.Profile/Domain/Services/ProfileService.cs
+++ Talent.Services.Profile/Domain/Services/ProfileService.cs
@@ -387,8 +387,36 @@ namespace Talent.Services.Profile.Domain.Services
 
         public async Task<IEnumerable<TalentSnapshotViewModel>> GetTalentSnapshotList(string employerOrJobId, bool forJob, int position, int increment)
         {
-            //Your code here;
-            throw new NotImplementedException();
+            if (position >= _userRepository.Collection.Count()) return null;
+            var talentList = _userRepository.Collection.Skip(position).Take(increment).AsEnumerable();
+            var result = new List<TalentSnapshotViewModel>();
+            var videoUrl = "";
+            var cvUrl = "";
+
+            foreach (var item in talentList)
+            {
+                videoUrl = string.IsNullOrWhiteSpace(item.VideoName)
+                    ? ""
+                    : await _fileService.GetFileURL(item.VideoName, FileType.UserVideo);
+                cvUrl = string.IsNullOrWhiteSpace(item.CvName)
+                    ? ""
+                    : await _fileService.GetFileURL(item.CvName, FileType.UserCV);
+                var newItem = new TalentSnapshotViewModel
+                {
+                    Id = item.Id,
+                    Name = item.FirstName + " " + item.LastName,
+                    PhotoId = item.ProfilePhotoUrl,
+                    VideoUrl = videoUrl,
+                    CVUrl = cvUrl,
+                    Summary = item.Summary,
+                    CurrentEmployment = "",
+                    Visa = item.VisaStatus,
+                    Level = "",
+                    Skills = item.Skills.Select(x => x.Skill).ToList()
+                };
+                result.Add(newItem);
+            }
+            return result;
         }
 
         public async Task<IEnumerable<TalentSnapshotViewModel>> GetTalentSnapshotList(IEnumerable<string> ids)
