e4f2c4af7a8d11f47ded2463b5bf5a89a548bf05
Leo Zhou <leo.zhou1986@hotmail.com>
2019-09-23 11:05:43 +1200

    Update Photo display

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Profile/PhotoUpload.jsx
@@ -71,19 +71,18 @@ export default class PhotoUpload extends Component {
     render() {
         let button = null;
         if (this.state.file) {
-            button = <Grid.Row centered columns={2}>
-                <Grid.Column>
-                    <Button
-                        type='reset'
-                        compact
-                        color='teal'
-                        onClick={this.handleUploadImage}
-                    >
-                        <Icon name='upload' />
-                        Upload
-                    </Button>
-                </Grid.Column>
-            </Grid.Row>
+            button = <div>
+                <br/>
+                <Button
+                    type='reset'
+                    compact
+                    color='teal'
+                    onClick={this.handleUploadImage}
+                >
+                    <Icon name='upload' />
+                    Upload
+                </Button>
+            </div>
         }
 
         let imageView;
@@ -107,25 +106,16 @@ export default class PhotoUpload extends Component {
         }
 
         return (
-            <Grid.Row>
-                <Grid.Column width={5}>
-                    <Grid>
-                        <Grid.Row centered columns={2}>
-                            <Grid.Column>
-                                <input
-                                    type="file"
-                                    ref={(ref) => this.upload = ref}
-                                    style={{ display: 'none' }}
-                                    onChange={this.handleImageChange}
-                                />
-                                {imageView}
-                                <Photo url={this.state.profilePhotoUrl}/>
-                            </Grid.Column>
-                        </Grid.Row>
-                        {button}
-                    </Grid>
-                </Grid.Column>
-            </Grid.Row>
+            <Grid.Column textAlign='center'>
+                <input
+                    type="file"
+                    ref={(ref) => this.upload = ref}
+                    style={{ display: 'none' }}
+                    onChange={this.handleImageChange}
+                />
+                {imageView}
+                {button}
+            </Grid.Column>
         )
     }
 }
@@ -135,59 +125,4 @@ PhotoUpload.propTypes = {
     updateProfileData: PropTypes.func.isRequired,
 };
 
-export class Photo extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            sw: 112,
-            sh: 112,
-            left: 0,
-            top:0
-        }
-        this.loadImage = this.loadImage.bind(this);
-    };
-
-    loadImage(e) {
-        console.log(e.target);
-        console.log(e.target.naturalWidth);
-        console.log(e.target.naturalHeight);
-        let nw = e.target.naturalWidth;
-        let nh = e.target.naturalHeight;
-        console.log("natural: ", nw, nh);
-        let dw = 112;
-        let dh = 112;
-        console.log("display: ", dw, dh);
-        let scale = Math.max(dw / nw, dh / nh);
-        let sw = Math.round(nw * scale);
-        let sh = Math.round(nh * scale);
-        let left = Math.round((dw - sw) / 2);
-        let top = Math.round((dh - sh) / 2);
-        console.log("show: ", sw, sh);
-        this.setState({ sw: sw, sh: sh, left: left, top: top });
-    }
-    render() {
-        return (
-            <div
-                color='black'
-                style=
-                {{
-                    border: '5px solid black',
-                    width: '112px',
-                    height: '112px'
-                }}
-            >
-                <img
-                    className='ui circular image'
-                    src={this.props.url}
-                    onLoad={this.loadImage}
-                    style=
-                    {{
-                        position: 'absolute'
-                    }}
-                />
-            </div>
-        )
-    }
-}
 
